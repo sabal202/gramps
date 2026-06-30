sabal202/gramps

commit d24aea791ef72bfb1bb1301de4bceddf4698d676

l10n(ru): fix relationship-calculator terms + Home Person wording
Downstream-only Russian locale corrections (not for upstream PR — these
belong in Weblate):

rel_ru.py: fix свёкр→свёкор typo; replace the 'великий дядя/тётя' calques
of great-uncle/aunt with the correct Russian grandparent's-sibling terms
(двоюродный дедушка/бабушка); fix genitive-plural тёть→тётей; fix
одиннацати→одиннадцати and двадцатитрёхюродный spelling; shift the
cousin-degree index by +1 for a (great-)grandparent's sibling in both the
singular and plural name builders.

po/ru.po: reword 'Home Person' family (Я (точка отсчёта) etc.), use neutral
child-reference type nouns (Усыновление/Воспитание/...), and fix the
mistranslated 'Given name' (Имя, not Настоящее имя).

Signed-off-by: Sabalevskiy Sergey <sabal2000@mail.ru>

gramps/plugins/rel/rel_ru.py
@@ -333,7 +333,7 @@ class Ancestor(Direct):
                 {  # by in_law_a
                     True: {  # by gender_a
                         Person.MALE: ["тесть", "тестя"],
-                        Person.FEMALE: ["свёкр", "свёкра"],
+                        Person.FEMALE: ["свёкор", "свёкора"],
                     },
                 },
             ],
@@ -365,7 +365,7 @@ class Ancestor(Direct):
                 {  # by in_law_a
                     True: {  # by gender_a
                         Person.MALE: ["тесть или тёща", "тестя или тёщи"],
-                        Person.FEMALE: ["свёкр или свекровь", "свёкра или свекрови"],
+                        Person.FEMALE: ["свёкор или свекровь", "свёкора или свекрови"],
                     },
                 },
             ],
@@ -527,7 +527,7 @@ class Collateral(Relative):
             ["восьмиюродный ", "восьмиюродного "],
             ["девятиюродный ", "девятиюродного "],
             ["десятиюродный ", "десятиюродного "],
-            ["одиннацатиюродный ", "одиннацатиюродного "],
+            ["одиннадцатиюродный ", "одиннадцатиюродного "],
             ["двенадцатиюродный ", "двенадцатиюродного "],
             ["тринадцатиюродный ", "тринадцатиюродного "],
             ["четырнадцатиюродный ", "четырнадцатиюродного "],
@@ -539,7 +539,7 @@ class Collateral(Relative):
             ["двадцатиюродный ", "двадцатиюродного "],
             ["двадцатиодноюродный ", "двадцатиюродного "],
             ["двадцатидвухюродный ", "двадцатидвухюродного "],
-            ["двадцатиютрехродный ", "двадцатитрехюродного "],
+            ["двадцатитрехюродный ", "двадцатитрехюродного "],
             ["двадцатичетырехюродный ", "двадцатичетырехюродного "],
             ["двадцатипятиюродный ", "двадцатипятиюродного "],
             ["двадцатишестиюродный ", "двадцатишестиюродного "],
@@ -556,7 +556,7 @@ class Collateral(Relative):
             ["восьмиюродная ", "восьмиюродной "],
             ["девятиюродная ", "девятиюродной "],
             ["десятиюродная ", "десятиюродной "],
-            ["одиннацатиюродная ", "одиннацатиюродной "],
+            ["одиннадцатиюродная ", "одиннадцатиюродной "],
             ["двенадцатиюродная ", "двенадцатиюродной "],
             ["тринадцатиюродная ", "тринадцатиюродной "],
             ["четырнадцатиюродная ", "четырнадцатиюродной "],
@@ -568,7 +568,7 @@ class Collateral(Relative):
             ["двадцатиюродная ", "двадцатиюродной "],
             ["двадцатиодноюродная ", "двадцатиюродной "],
             ["двадцатидвухюродная ", "двадцатидвухюродной "],
-            ["двадцатиютрехродная ", "двадцатитрехюродной "],
+            ["двадцатитрехюродная ", "двадцатитрехюродной "],
             ["двадцатичетырехюродная ", "двадцатичетырехюродной "],
             ["двадцатипятиюродная ", "двадцатипятиюродной "],
             ["двадцатишестиюродная ", "двадцатишестиюродной "],
@@ -587,7 +587,7 @@ class Collateral(Relative):
         ["восьмиюродные ", "восьмиюродных "],
         ["девятиюродные ", "девятиюродных "],
         ["десятиюродные ", "десятиюродных "],
-        ["одиннацатиюродные ", "одиннацатиюродных "],
+        ["одиннадцатиюродные ", "одиннадцатиюродных "],
         ["двенадцатиюродные ", "двенадцатиюродных "],
         ["тринадцатиюродные ", "тринадцатиюродных "],
         ["четырнадцатиюродные ", "четырнадцатиюродных "],
@@ -599,7 +599,7 @@ class Collateral(Relative):
         ["двадцатиюродные ", "двадцатиюродных "],
         ["двадцатиодноюродные ", "двадцатиюродных "],
         ["двадцатидвухюродные ", "двадцатидвухюродных "],
-        ["двадцатиютрехродные ", "двадцатитрехюродных "],
+        ["двадцатитрехюродные ", "двадцатитрехюродных "],
         ["двадцатичетырехюродные ", "двадцатичетырехюродных "],
         ["двадцатипятиюродные ", "двадцатипятиюродных "],
         ["двадцатишестиюродные ", "двадцатишестиюродных "],
@@ -714,10 +714,15 @@ class Collateral(Relative):
             degrees = self.degrees_plural
 
             stereotyped = self.stereotyped_plurals[self.lvl]
-            if self.dgr >= len(degrees):
+            # RU: (great-)grandparent's siblings shift cousin-degree by +1
+            if isinstance(self, Senior) and self.lvl == 2:
+                _dgr_idx = self.dgr
+            else:
+                _dgr_idx = self.dgr - 1
+            if _dgr_idx >= len(degrees):
                 degree = degrees[-1][genitive]
             else:
-                degree = degrees[self.dgr - 1][genitive]
+                degree = degrees[_dgr_idx][genitive]
 
             stereotyped_name = stereotyped[genitive].format(degree)
 
@@ -894,24 +899,24 @@ class Senior(Collateral):
         Person.MALE: [  # gender_b is Male
             [None, None],
             ["{}дядя", "{}дяди"],
-            ["{}великий дядя", "{}великого дяди"],
+            ["{}дедушка", "{}дедушки"],
         ],
         Person.FEMALE: [  # gender_b is Female
             [None, None],
             ["{}тётя", "{}тёти"],
-            ["{}великая тётя", "{}великой тёти"],
+            ["{}бабушка", "{}бабушки"],
         ],
         Person.UNKNOWN: [  # gender_b is Male
             [None, None],
             ["{}дядя или тётя", "{}дяди или тёти"],
-            ["{}великий дядя или тётя", "{}великого дяди или тёти"],
+            ["{}дедушка или бабушка", "{}дедушки или бабушки"],
         ],
     }
 
     stereotyped_plurals = [
         [None, None],
-        ["{}дяди и тёти", "{}дядей и тёть"],
-        ["{}великие дяди и тёти", "{}великих дядей и тёть"],
+        ["{}дяди и тёти", "{}дядей и тётей"],
+        ["{}дедушки и бабушки", "{}дедушек и бабушек"],
     ]
 
     def _get_stereotyped_name(self, genitive=False):
@@ -940,10 +945,17 @@ class Senior(Collateral):
 
             stereotyped = self.stereotyped[gender_b][self.Ga - self.Gb]
 
-            if self.Gb >= len(degrees):
+            # RU: sibling of a (great-)grandparent => "двоюродный дедушка/бабушка"
+            # (Gb=1), "троюродный..." (Gb=2); cousin-degree shifts by +1.
+            if self.Ga - self.Gb == 2:
+                _deg_idx = self.Gb
+            else:
+                _deg_idx = self.Gb - 1
+
+            if _deg_idx >= len(degrees):
                 degree = degrees[-1][genitive]
             else:
-                degree = degrees[self.Gb - 1][genitive]
+                degree = degrees[_deg_idx][genitive]
 
             stereotyped_name = stereotyped[genitive].format(degree)
 

po/ru.po
@@ -5714,7 +5714,7 @@ msgstr "Выбирает детей соответствующих фильтр�
 
 #: ../gramps/gen/filters/rules/person/_isdefaultperson.py:54
 msgid "Home Person"
-msgstr "Базовое лицо"
+msgstr "Я (точка отсчёта)"
 
 #: ../gramps/gen/filters/rules/person/_isdefaultperson.py:56
 msgid "Matches the Home Person"
@@ -7098,15 +7098,15 @@ msgstr "Усыновлённый"
 
 #: ../gramps/gen/lib/childreftype.py:76
 msgid "Stepchild"
-msgstr "Пасынок/Падчерица"
+msgstr "Сводное родство"
 
 #: ../gramps/gen/lib/childreftype.py:77
 msgid "Sponsored"
-msgstr "Опекаемый"
+msgstr "Опекунство"
 
 #: ../gramps/gen/lib/childreftype.py:78
 msgid "Foster"
-msgstr "Воспитанник"
+msgstr "Воспитание"
 
 #: ../gramps/gen/lib/citation.py:106 ../gramps/gen/lib/notetype.py:90
 #: ../gramps/gui/clipboard.py:481 ../gramps/gui/configure.py:810
@@ -7821,7 +7821,7 @@ msgstr "Иное"
 
 #: ../gramps/gen/lib/eventtype.py:199
 msgid "Adopted"
-msgstr "Усыновлённый"
+msgstr "Усыновление"
 
 #: ../gramps/gen/lib/eventtype.py:200 ../gramps/gen/utils/symbols.py:93
 #: ../gramps/gui/configure.py:2797 ../gramps/gui/merge/mergeperson.py:205
@@ -8587,7 +8587,7 @@ msgstr "Полное Имя"
 
 #: ../gramps/gen/lib/name.py:180 ../gramps/plugins/importer/importcsv.py:206
 msgid "Given name"
-msgstr "Настоящее имя"
+msgstr "Имя"
 
 #: ../gramps/gen/lib/name.py:184 ../gramps/plugins/webreport/basepage.py:2058
 #: ../gramps/plugins/webreport/basepage.py:2116
@@ -12537,7 +12537,7 @@ msgstr "Имя и ID активного лица"
 #: ../gramps/gui/configure.py:1645
 #: ../gramps/plugins/textreport/indivcomplete.py:394
 msgid "Relationship to home person"
-msgstr "Отношение к базовому лицу"
+msgstr "Кем приходится мне"
 
 #: ../gramps/gui/configure.py:1656
 msgid "Status bar"
@@ -15278,7 +15278,7 @@ msgstr "Установить активное лицо"
 
 #: ../gramps/gui/editors/editperson.py:787
 msgid "Make Home Person"
-msgstr "Установить базовое лицо"
+msgstr "Сделать точкой отсчёта"
 
 #: ../gramps/gui/editors/editperson.py:906
 msgid "Problem changing the gender"
@@ -20917,7 +20917,7 @@ msgstr "Нельзя создать закладку: никто не выдел
 
 #: ../gramps/gui/views/navigationview.py:323
 msgid "No Home Person"
-msgstr "Базовое лицо не назначено"
+msgstr "Точка отсчёта не выбрана"
 
 #: ../gramps/gui/views/navigationview.py:325
 msgid ""
@@ -25365,7 +25365,7 @@ msgstr ""
 
 #: ../gramps/plugins/gramplet/whatsnext.py:162
 msgid "No Home Person set."
-msgstr "Базовое лицо не назначено."
+msgstr "Точка отсчёта не выбрана."
 
 #: ../gramps/plugins/gramplet/whatsnext.py:345
 msgid "first name unknown"
@@ -31433,7 +31433,7 @@ msgstr "Редактор фильтров людей"
 #: ../gramps/plugins/lib/libpersonview.py:381
 #: ../gramps/plugins/view/pedigreeview.py:1781
 msgid "Set _Home Person"
-msgstr "У_становить базовое лицо"
+msgstr "Сделать точкой отсчёта"
 
 #: ../gramps/plugins/lib/libpersonview.py:286
 #: ../gramps/plugins/view/fanchartview.py:184

commit 161ddeee9f1e9c0bb789db6569ca6c88b6894929

l10n(ru): fix свёкор genitive свёкра and revert тёть genitive plural
Two grammar regressions from d24aea791:

- свёкор has a fleeting vowel, so the genitive is "свёкра" (not "свёкора"):
  fix both occurrences ["свёкор", "свёкра"] / "свёкра или свекрови".
- "тёть" is a valid genitive plural of тётя (Wiktionary lists "тёть // тётей"
  as equal variants), so the тётей change was unnecessary churn — revert it.

Verified against ru.wiktionary.org and gramota.ru.

Signed-off-by: Sabalevskiy Sergey <sabal2000@mail.ru>

gramps/plugins/rel/rel_ru.py
@@ -333,7 +333,7 @@ class Ancestor(Direct):
                 {  # by in_law_a
                     True: {  # by gender_a
                         Person.MALE: ["тесть", "тестя"],
-                        Person.FEMALE: ["свёкор", "свёкора"],
+                        Person.FEMALE: ["свёкор", "свёкра"],
                     },
                 },
             ],
@@ -365,7 +365,7 @@ class Ancestor(Direct):
                 {  # by in_law_a
                     True: {  # by gender_a
                         Person.MALE: ["тесть или тёща", "тестя или тёщи"],
-                        Person.FEMALE: ["свёкор или свекровь", "свёкора или свекрови"],
+                        Person.FEMALE: ["свёкор или свекровь", "свёкра или свекрови"],
                     },
                 },
             ],
@@ -915,7 +915,7 @@ class Senior(Collateral):
 
     stereotyped_plurals = [
         [None, None],
-        ["{}дяди и тёти", "{}дядей и тётей"],
+        ["{}дяди и тёти", "{}дядей и тёть"],
         ["{}дедушки и бабушки", "{}дедушек и бабушек"],
     ]
 